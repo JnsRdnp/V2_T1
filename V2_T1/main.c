c9b208b15d28d0df256f8d2f1649db7960da0d19
JnsRdnp <joonasroska123@gmail.com>
2022-09-02 16:25:05 +0300

    Valmis tehtävä

--- V2_T1/main.c
+++ V2_T1/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+//Ohjelma kysyy käyttäjältä kokonaisluvun. Jos käyttäjän
+//antama luku on pienempi, kuin 10 saa käyttäjä
+//ilmoituksen "Annoit luvun joka pienempi kuin 10"
 
 int main()
 {
-    printf("Hello World!\n");
+    int nmr;
+
+    printf("Kerro jokin kokonaisluku\n");
+    scanf("%d",&nmr);
+
+    if (nmr<10)
+    {
+        printf("Annoit luvun joka pienempi kuin kymmenen\n");
+    }
+    else if(nmr==10)
+    {
+        printf("Annoit numeron kymmenen\n");
+    }
+    else
+    {
+        printf("Annoit numeron joka on suurempi, kuin kymmenen\n");
+    }
+
     return 0;
 }
